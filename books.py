b765a126003fb7a4a4d0988857908473ecdbc70e
EunBong Lee <eunbong73.lee@gmail.com>
2022-04-01 22:54:30 +0900

    add Enumeration Path

--- books.py
+++ books.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-
+from enum import Enum
 app = FastAPI()
 
 BOOKS = {
@@ -10,9 +10,25 @@ BOOKS = {
     'book_5': {'title': 'Title Five', 'author': 'Author Five'},
 }
 
+class DirectionName (str, Enum):
+    north = "North"
+    south = "South"
+    east  = "East"
+    west  = "West"
+
 @app.get("/")
 async def read_all_books():
     return BOOKS
+
+@app.get("/directions/{direction_name}")
+async def get_direction(direction_name: DirectionName):
+    if (direction_name == DirectionName.north):
+        return {"Direction": direction_name, "sub": "Up"}
+    if (direction_name == DirectionName.south):
+        return {"Direction": direction_name, "sub": "Down"}
+    if (direction_name == DirectionName.east):
+        return {"Direction": direction_name, "sub": "Right"}
+    return {"Direction": direction_name, "sub": "Left"}
 
 
 @app.get("/books/mybook")
