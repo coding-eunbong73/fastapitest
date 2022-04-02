0cbcf273169c39809e02207fe2c78e1efcf3e108
EunBong Lee <eunbong73.lee@gmail.com>
2022-04-02 12:48:00 +0900

    add book2.py(get, post), execute --> uvicorn books2:app --reload

---
+++ books2.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from uuid import UUID
+
+app = FastAPI()
+
+class Book(BaseModel):
+    id: UUID
+    title: str
+    author: str
+    description: str
+    rating: int
+
+BOOKS = []
+
+@app.get("/")
+async def read_all_books():
+    return BOOKS
+
+@app.post("/")
+async def create_book(book:Book):
+    BOOKS.append(book)
+    return book
