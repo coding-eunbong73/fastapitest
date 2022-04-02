ffa28d3dcf56b2391f520caec32c6121ad7eed65
EunBong Lee <eunbong73.lee@gmail.com>
2022-04-02 10:21:07 +0900

    add post, put, delete

--- books.py
+++ books.py
@@ -53,3 +53,29 @@ async def read_favorite_book():
 async def read_book(book_name:str):
     return BOOKS[book_name]
 
+
+@app.post("/")
+async def create_book(book_title, book_author):
+    current_book_id = 0
+
+    if len(BOOKS) > 0:
+        for book in BOOKS:
+            x = int(book.split('_')[-1])
+            if x > current_book_id:
+                current_book_id = x
+
+    BOOKS[f'book_{current_book_id + 1}'] = {'title': book_title, 'author': book_author}
+    return BOOKS[f'book_{current_book_id + 1}']
+
+
+@app.put("/{book_name}")
+async def update_book(book_name: str, book_title: str, book_author: str):
+    book_information = {'title': book_title, 'author': book_author}
+    BOOKS[book_name] = book_information
+    return book_information
+
+
+@app.delete("/{book_name}")
+async def delete_book(book_name):
+    del BOOKS[book_name]
+    return f'Book {book_name} deleted.'
