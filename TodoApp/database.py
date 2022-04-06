0d37016b9a2e50e2ea68ccd487f3907a880950f0
EunBong Lee <eunbong73.lee@gmail.com>
2022-04-06 18:12:44 +0900

    pip 설치 library 정리, postgresql관련 설정 및 db관련 내용 정리

--- TodoApp/database.py
+++ TodoApp/database.py
@@ -2,10 +2,20 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./todos.db"
+#sqlite3
+#SQLALCHEMY_DATABASE_URL = "sqlite:///./todos.db"
+
+#postgresql
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:New1234!!@localhost/TodoApplicationDatabase"
+
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    # sqlite3
+    # SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+
+    # postgresql
+    SQLALCHEMY_DATABASE_URL
+
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
