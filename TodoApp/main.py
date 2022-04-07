9a002ac08fde19f52a59c54ec472c967ae9ddbd6
EunBong Lee <eunbong73.lee@gmail.com>
2022-04-07 19:16:45 +0900

    external router 설정

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, Depends, HTTPException
 import models
 from database import engine, SessionLocal
 from routers import auth, todos
+from companyapis import companyapis
 
 app = FastAPI()
 
@@ -9,3 +10,9 @@ models.Base.metadata.create_all(bind=engine)
 
 app.include_router(auth.router)
 app.include_router(todos.router)
+app.include_router(
+    companyapis.router,
+    prefix="/companyapis",
+    tags=["companyapis"],
+    responses={418: {"description": "Internal Use Only"}}
+)
