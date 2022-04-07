fe904c89e6fa2fda281a5fc504924d77e3c19b49
EunBong Lee <eunbong73.lee@gmail.com>
2022-04-07 19:32:44 +0900

    dependency routing 설정 ( 특정 경로로 요청오는 헤더값을 체크하여 처리하는 로직 )

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, Depends, HTTPException
 import models
 from database import engine, SessionLocal
 from routers import auth, todos
-from companyapis import companyapis
+from companyapis import companyapis, dependencies
 
 app = FastAPI()
 
@@ -14,5 +14,6 @@ app.include_router(
     companyapis.router,
     prefix="/companyapis",
     tags=["companyapis"],
+    dependencies=[Depends (dependencies.get_token_header)],
     responses={418: {"description": "Internal Use Only"}}
 )
