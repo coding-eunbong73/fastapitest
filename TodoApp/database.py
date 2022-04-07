bd5620654154b3b51f5ac1e47175d0ebea6915b5
EunBong Lee <eunbong73.lee@gmail.com>
2022-04-07 11:05:54 +0900

    mysql 관련 설정 및 library 추가

--- TodoApp/database.py
+++ TodoApp/database.py
@@ -6,14 +6,16 @@ from sqlalchemy.ext.declarative import declarative_base
 #SQLALCHEMY_DATABASE_URL = "sqlite:///./todos.db"
 
 #postgresql
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:New1234!!@localhost/TodoApplicationDatabase"
+#SQLALCHEMY_DATABASE_URL = "postgresql://postgres:New1234!!@localhost/TodoApplicationDatabase"
 
+#mysql
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:New1234!!@127.0.0.1:3306/todoapp"
 
 engine = create_engine(
     # sqlite3
     # SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
 
-    # postgresql
+    # postgresql, mysql
     SQLALCHEMY_DATABASE_URL
 
 )
