8cc006497388225bc2de4bba90ce0fc9fd3aeccd
EunBong Lee <eunbong73.lee@gmail.com>
2022-04-11 16:09:26 +0900

    root 패스 추가 및 heroku postgresql 환경 설정 작업

--- database.py
+++ database.py
@@ -1,15 +1,25 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./todos.db"
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL");
+if SQLALCHEMY_DATABASE_URL is None:
+    SQLALCHEMY_DATABASE_URL = "sqlite:///./todos.db"
+else:
+    if SQLALCHEMY_DATABASE_URL.startswith("postgres://"):
+        SQLALCHEMY_DATABASE_URL = SQLALCHEMY_DATABASE_URL.replace("postgres://", "postgresql://", 1)
+
+print(SQLALCHEMY_DATABASE_URL)
 
 # MYSQL Series
 # SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:test1234!@127.0.0.1:3306/todoapp"
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+if SQLALCHEMY_DATABASE_URL is None:
+    engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+else:
+    engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 # MYSQL Series
 # engine = create_engine(
--- main.py
+++ main.py
@@ -3,12 +3,18 @@ import models
 from database import engine
 from routers import auth, todos
 from starlette.staticfiles import StaticFiles
+from starlette import status
+from starlette.responses import RedirectResponse
 
 app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
+
+@app.get("/")
+async def root():
+    return RedirectResponse(url="/todos", status_code=status.HTTP_302_FOUND)
 
 app.include_router(auth.router)
 app.include_router(todos.router)
